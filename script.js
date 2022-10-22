d981050c97d426b441afd70208eb420c5dbc446c
mohammedalgamal <mohammedalgamal95@gmail.com>
2022-10-22 09:54:02 +0200

    Allow the user to determine number of pixels

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function rnadom(start, end) {
 
 function makeGrid(num=16){
     parent = document.querySelector('.container');
-    let equation = (416 - 2*num) / num;
-    console.log(equation); 
+    parent.textContent = '';
+    let equation = (416 - 2*num) / num; 
     for (let i = 1; i <= num*num; i++) {
         const cdiv = document.createElement('div');
         cdiv.style.cssText = `width: ${equation}px; height: ${equation}px;`;
@@ -20,5 +20,21 @@ function colorGrid(){
         e.target.style.cssText += "background-color: black;";
     }));
 }
-makeGrid();
-colorGrid();
+
+function getDimensions() {
+    const btn = document.querySelector('button');
+    let dimension = 16;
+    makeGrid();
+    colorGrid();
+    btn.addEventListener('click', e => {
+        dimension = prompt('How many pixels do you want in one side?');
+        if (dimension >= 100) {
+            dimension = 100;
+        }
+        makeGrid(dimension);
+        colorGrid();
+    });
+}
+
+getDimensions();
+

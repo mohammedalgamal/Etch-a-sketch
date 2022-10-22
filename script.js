72e042f70fcc040c37e3b7e8c30e09cfab697d21
mohammedalgamal <mohammedalgamal95@gmail.com>
2022-10-22 09:13:39 +0200

    Make the grid size flexible

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-parent = document.querySelector('.container');
-for (let i = 1; i <= 256; i++) {
-    const cdiv = document.createElement('div');
-    cdiv.className = `inside`;
-    parent.appendChild(cdiv);
-};
+function rnadom(start, end) {
+    return Math.floor((Math.random() * (end - start))) + start;
+}
 
-let divs = document.querySelectorAll('.inside');
+function makeGrid(num=16){
+    parent = document.querySelector('.container');
+    let equation = (416 - 2*num) / num;
+    console.log(equation); 
+    for (let i = 1; i <= num*num; i++) {
+        const cdiv = document.createElement('div');
+        cdiv.style.cssText = `width: ${equation}px; height: ${equation}px;`;
+        cdiv.className = 'inside';
+        parent.appendChild(cdiv);
+    };
+}
 
-divs.forEach(div => div.addEventListener('mouseenter', (e) => {
-    e.target.style.cssText = "background-color: black;";
-}));
+function colorGrid(){
+    let divs = document.querySelectorAll('.inside');
+    divs.forEach(div => div.addEventListener('mouseenter', (e) => {
+        e.target.style.cssText += "background-color: black;";
+    }));
+}
+makeGrid();
+colorGrid();
